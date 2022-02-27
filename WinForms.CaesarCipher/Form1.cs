4f220bca6ca714e756a626b6c30e863d71f14103
Kyoungtaek Oh <okts83@gmail.com>
2022-02-27 11:29:39 +0900

    WinForms.CaesarCipher
    Add textbox validation

--- WinForms.CaesarCipher/Form1.cs
+++ WinForms.CaesarCipher/Form1.cs
@@ -29,11 +29,23 @@ namespace WinForms.CaesarCipher
 
         private void btnEcrypt_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(tbxPlain.Text))
+            {
+                MessageBox.Show("Please input text.");
+                return;
+            }
+
             tbxCipher.Text = caesarCipher.Encode(tbxPlain.Text.Trim(), (int)shiftKey.Value);
         }
 
         private void btnDecryption_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(tbxCipher.Text))
+            {
+                MessageBox.Show("Please input text.");
+                return;
+            }
+
             tbxPlain.Text = caesarCipher.Decode(tbxCipher.Text.Trim(), (int)shiftKey.Value);
         }
     }
