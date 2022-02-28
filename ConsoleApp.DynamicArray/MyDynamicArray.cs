b8b36d38955c070c11a293a09c4ead3c6cea229e
Kyoungtaek Oh <okts83@gmail.com>
2022-03-01 06:53:00 +0900

    ConsoleApp.DynamicArray

---
+++ ConsoleApp.DynamicArray/MyDynamicArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp.DynamicArray
+{
+    public class MyDynamicArray
+    {
+        private object[] myArray;
+        private int count;
+        private int capacity;
+        public int Count
+        {
+            get { return this.count; }
+        }
+        public int Capacity
+        {
+            get { return this.capacity; }
+        }
+
+        public MyDynamicArray()
+        {
+            this.capacity = 2;
+            myArray = new object[this.Capacity];
+            this.count = 0;
+        }
+
+        public object this[int index]
+        {
+            get { return this.myArray[CheckOutOfRange(index)]; }
+            set { this.myArray[CheckOutOfRange(index)] = value; }
+        }
+
+        public void Add(object data)
+        {
+            if (this.count == this.Capacity)
+            {
+                CreateNewArray();
+            }
+            this.myArray[this.count] = data;
+            this.count++;
+        }
+
+        private void CreateNewArray()
+        {
+            this.capacity *= 2;
+            object[] temp = myArray;
+            myArray = new object[this.capacity];
+
+            Array.Copy(temp, myArray, temp.Length);
+        }
+
+        private int CheckOutOfRange(int index)
+        {
+            if (index < 0 || index >= this.count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return index;
+        }
+    }
+}
---
+++ ConsoleApp.DynamicArray/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp.DynamicArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var da = new MyDynamicArray();
+            Console.WriteLine("Initialization");
+            Console.WriteLine("Count:{0}, Capacity:{1}", da.Count, da.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine("Add 1 items");
+            da.Add(1); 
+            Console.WriteLine("Count:{0}, Capacity:{1}", da.Count, da.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine("Add 2 more items");
+            da.Add(2); da.Add(3);
+            Console.WriteLine("Count:{0}, Capacity:{1}", da.Count, da.Capacity);
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine("Before myArray[2]: {0}", da[2]);
+                da[2] = "Test Indexer";
+                Console.WriteLine("After  myArray[2]: {0}", da[2]);
+
+                Console.WriteLine();
+                Console.WriteLine("Try to Add item, out of count.");
+                Console.WriteLine("Current index position: {0}, Target index: da[31]", da.Count);
+                da[31] = "Indexer 31";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Count:{0}, Capacity:{1}", da.Count, da.Capacity);
+            Console.WriteLine();
+
+            Console.WriteLine("Add 1 items");
+            da.Add("final");
+            Console.WriteLine("Count:{0}, Capacity:{1}", da.Count, da.Capacity);
+        }
+    }
+}
