a76d9e88c704f6a20ba6278b3b849fe2bb14edda
Kyoungtaek Oh <okts83@gmail.com>
2022-03-03 05:13:21 +0900

    Winforms.DataInputCSV
    Data to CSV file, append:true

---
+++ WinForms.DataInputCSV/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace WinForms.DataInputCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxPassportNo = new System.Windows.Forms.TextBox();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.dtpDateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Passport No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Date of Birth";
+            // 
+            // tbxPassportNo
+            // 
+            this.tbxPassportNo.Location = new System.Drawing.Point(202, 37);
+            this.tbxPassportNo.Name = "tbxPassportNo";
+            this.tbxPassportNo.Size = new System.Drawing.Size(240, 35);
+            this.tbxPassportNo.TabIndex = 3;
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(202, 113);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(240, 35);
+            this.tbxName.TabIndex = 4;
+            // 
+            // dtpDateOfBirth
+            // 
+            this.dtpDateOfBirth.Location = new System.Drawing.Point(202, 199);
+            this.dtpDateOfBirth.Name = "dtpDateOfBirth";
+            this.dtpDateOfBirth.Size = new System.Drawing.Size(240, 35);
+            this.dtpDateOfBirth.TabIndex = 5;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(30, 293);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(131, 40);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(311, 293);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(131, 40);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 353);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.dtpDateOfBirth);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.tbxPassportNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Input to CSV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxPassportNo;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.DateTimePicker dtpDateOfBirth;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ WinForms.DataInputCSV/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms.DataInputCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtpDateOfBirth.Format = DateTimePickerFormat.Short;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetField();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbxPassportNo.Text) || string.IsNullOrWhiteSpace(tbxName.Text))
+            {
+                MessageBox.Show("Passport Number and Name are requirement!");
+                return;
+            }
+
+            string passportNo = tbxPassportNo.Text.Trim();
+            string name = tbxName.Text.Trim();
+            string dob = dtpDateOfBirth.Value.ToShortDateString();
+            string personData = $"{passportNo},{name},{dob}";
+
+            WriteData(personData);
+        }
+
+        private void WriteData(string personData)
+        {
+            string path = ConfigurationManager.AppSettings["Output"];
+            Debug.WriteLine(path);
+
+            var write = new StreamWriter(path, append: true);
+            write.WriteLine(personData);
+            write.Close();
+            MessageBox.Show("Saved Successfully!");
+
+            ResetField();
+        }
+
+        private void ResetField()
+        {
+            tbxPassportNo.Clear();
+            tbxName.Clear();
+            dtpDateOfBirth.Value = DateTime.Now;
+            tbxPassportNo.Focus();
+        }
+    }
+}
