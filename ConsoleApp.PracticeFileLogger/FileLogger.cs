d6a718a2223f1bdd4d6ef7902ee69922aab780f7
Kyoungtaek Oh <okts83@gmail.com>
2022-03-03 12:50:37 +0900

    ConsoleApp.PracticeFileLogger
    ToArray(), Reverse()

---
+++ ConsoleApp.PracticeFileLogger/FileLogger.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace ConsoleApp.PracticeFileLogger
+{
+    public class FileLogger
+    {
+        private string filePath;
+        public FileLogger(string filePath)
+        {
+            if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+            }
+
+            this.filePath = filePath;
+        }
+
+        public int LogSize
+        {
+            get
+            {
+                var fi = new FileInfo(this.filePath);
+                return (int)fi.Length;
+            }
+        }
+        public string LogFile
+        {
+            get { return Path.GetFileName(this.filePath); }
+        }
+
+        public void Write(string msg)
+        {
+            File.AppendAllText(this.filePath, $"{DateTime.Now} {msg}");
+        }
+
+        public void WriteLine(string msg)
+        {
+            File.AppendAllText(this.filePath, $"{DateTime.Now} {msg}{Environment.NewLine}");
+        }
+
+        public List<string> Find(string keyword)
+        {
+            using(var rd = new StreamReader(this.filePath))
+            {
+                var result = new List<string>();
+                string line;
+
+                while ((line = rd.ReadLine()) != null)
+                {
+                    if (line.IndexOf(keyword) != -1)
+                    {
+                        result.Add(line);
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        public List<string> GetTopN(int n)
+        {
+            return GetAll().Take(n).ToList();
+        }
+
+        public List<string> GetLastN(int n)
+        {
+            string[] temp = GetAll().ToArray();
+
+            return temp.Reverse().Take(n).ToList();
+        }
+        private List<string> GetAll()
+        {
+            return File.ReadAllLines(this.filePath).ToList();
+        }
+
+    }
+}
---
+++ ConsoleApp.PracticeFileLogger/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp.PracticeFileLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Logs\SystemLog.txt";
+
+            var fileLogger = new FileLogger(path);
+
+            fileLogger.WriteLine("Service Started.");
+            fileLogger.Write("Memory error occurred.");
+            fileLogger.Write("Network error occurred.");
+            fileLogger.WriteLine("Memory error occurred.");
+            fileLogger.WriteLine("1.Memory error occurred.");
+            fileLogger.WriteLine("2.Memory error occurred.");
+            fileLogger.WriteLine("3.Memory error occurred.");
+            fileLogger.WriteLine("4.Memory error occurred.");
+            fileLogger.WriteLine("5.Memory error occurred.");
+
+
+            Console.WriteLine("-----Start Find Keyword: Memory");
+            foreach (string findKeyword in fileLogger.Find("Memory"))
+            {
+                Console.WriteLine(findKeyword);
+            }
+
+            Console.WriteLine("-----End Find Keyword");
+            Console.WriteLine();
+
+            // Get Top(N) lines from file
+            Console.WriteLine("-----Start GetTop(N)");
+            foreach (string item in fileLogger.GetTopN(2))
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----End GetTop(N)");
+            Console.WriteLine();
+
+            // Get Bottom(N) lines from file
+            Console.WriteLine("-----Start GetLast(N)");
+            foreach (string item in fileLogger.GetLastN(5))
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----End GetLast(N)");
+            Console.WriteLine();
+
+            Console.WriteLine($"Logfile Name: {fileLogger.LogFile}");
+            Console.WriteLine($"Logfile Size: {fileLogger.LogSize}");
+
+        }
+    }
+}
