b5f3cdce69f2d92c1d67fb645c6c36913e37c547
Kyoungtaek Oh <okts83@gmail.com>
2022-03-07 13:09:00 +0900

    ConsoleApp.SQLBasic

---
+++ ConsoleApp.SQLBasic/DataAccess.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ConsoleApp.SQLBasic
+{
+    public class DataAccess
+    {
+        private string connectionString;
+
+        public DataAccess(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public string GetAll()
+        {
+            using(var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT * FROM Customer";
+
+                var result = new StringBuilder();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                while (sdr.Read())
+                {
+                    string customer = $"{sdr["Id"]} | {sdr["Name"]} | {sdr["Phone"]} {Environment.NewLine}";
+                    result.Append(customer);
+                }
+                sdr.Close();
+
+                return result.ToString();
+            }
+        }
+
+        public bool Insert(Customer customer)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    var cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = "INSERT INTO Customer VALUES(@Id, @Name, @Phone)";
+                    cmd.Parameters.AddWithValue("@Id", customer.Id);
+                    cmd.Parameters.AddWithValue("@Name", customer.Name);
+                    cmd.Parameters.AddWithValue("@Phone", customer.Phone);
+                    cmd.ExecuteNonQuery();
+
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                    return false;
+                }
+            }
+        }
+
+        public bool Update(string newPhone, string name)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand();
+                conn.Open();
+                cmd.Connection = conn;
+                cmd.CommandText = String.Format($"UPDATE Customer SET Phone=@Phone WHERE Name=@Name;");
+                cmd.Parameters.AddWithValue("@Phone", newPhone);
+                var paramName = new SqlParameter("@Name", System.Data.SqlDbType.NVarChar, 50);
+                paramName.Value = name;
+                cmd.Parameters.Add(paramName);
+
+                if (cmd.ExecuteNonQuery() == 1)
+                {
+                    return true;
+
+                }
+
+                return false;
+            }
+        }
+
+        public int Delete(int id)
+        {
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand();
+                conn.Open();
+                cmd.Connection = conn;
+                cmd.CommandText = String.Format($"DELETE Customer WHERE Id=@Id");
+                cmd.Parameters.AddWithValue("@Id", id);
+                int resultCount = cmd.ExecuteNonQuery();
+
+                return resultCount;
+            }
+        }
+    }
+}
---
+++ ConsoleApp.SQLBasic/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApp.SQLBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var da = new DataAccess(GetConnectionString());
+
+            Customer[] customers = new Customer[]
+            {
+                new Customer { Id = 11, Name = "Kim", Phone = "010" },
+                new Customer { Id = 21, Name = "Lee", Phone = "010-511-4342" },
+                new Customer { Id = 3, Name = "Park", Phone = "010-532-2232" },
+                new Customer { Id = 41, Name = "Oh", Phone = "010-532-3232" },
+                new Customer { Id = 51, Name="','') DELETE Customer --"} // Wrong Data
+             };
+
+            Console.WriteLine("----- Insert");
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(da.Insert(customer) ? "Success" : "Fail");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("----- Update");
+            Console.WriteLine(da.Update("010-222-3333", "Kim") ? "Updated Successfully!" : "Update Failed");
+            Console.WriteLine();
+
+            Console.WriteLine("----- Delete");
+            int targetId = 3;
+            Console.WriteLine($"{da.Delete(targetId)} row(s) deleted.");
+            Console.WriteLine();
+
+
+            Console.WriteLine("----- Select");
+            Console.WriteLine(da.GetAll());
+        }
+
+        private static string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            return builder.Build().GetConnectionString("KTConnection");
+        }
+    }
+}
